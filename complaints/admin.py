659925b7a698f6d9b4677be0cc8ac7bccbfce693
Shamnaek <141479896+Shamnaek@users.noreply.github.com>
2026-05-11 13:58:31 +0530

    commit

--- complaints/admin.py
+++ complaints/admin.py
@@ -1,7 +1,45 @@
+# from django.contrib import admin
+# from .models import Profile, Complaint
+
+# admin.site.register(Profile)
+# admin.site.register(Complaint)
+
+# # Register your models here.
+
 from django.contrib import admin
 from .models import Profile, Complaint
 
-admin.site.register(Profile)
-admin.site.register(Complaint)
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'id',
+        'user',
+        'role'
+    ]
+
+
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'id',
+        'title',
+        'user',
+        'location',
+        'status',
+        # 'assigned_to',
+        # 'created_at'
+    ]
+
+    list_filter = [
+        'status',
+        'created_at'
+    ]
+
+    search_fields = [
+        'title',
+        'location',
+        'user__username'
+    ]
